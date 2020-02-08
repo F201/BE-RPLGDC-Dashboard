ba8f82284e51e35e27642e8c4ea90b6ab8815407
Aji P Santoso <ajisantoso12@gmail.com>
2020-02-08 15:42:02 +0700

    add handle disconnect

--- conn.js
+++ conn.js
@@ -1,15 +1,32 @@
-var mysql = require('mysql');
-
-var con = mysql.createPool({
+const mysql = require('mysql');
+const db_config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
-});
+};
+var con;
 
-con.connect(function (err){
-    if(err) throw err;
-});
+function handleDisconnect() {
+  con = mysql.createConnection(db_config);
+
+  con.connect(function (err){
+      if(err) {
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000);
+      }
+  });
+
+  con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+handleDisconnect();
 
 module.exports = con
 // ;rpl
